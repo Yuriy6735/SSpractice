805edd764803328441df0328a4fe4957b42dc6dc
Yuriy <yuriy.liskevich@gmail.com>
2019-05-04 23:02:02 +0300

    testing

--- functions/getPredictions/getPredictions/main.py
+++ functions/getPredictions/getPredictions/main.py
@@ -8,13 +8,13 @@ def get_predictions(request):
     city = request.data.decode("utf-8")
     if city == "test":
         ip_tf = os.environ.get("ip_tf")
-        r_to_tf = requests.post({0}, ip_tf, {1}, data=json.dumps(city) .format("http://", ":80"))
+        r_to_tf = requests.post('http://{0}:9000'.format(ip_tf), data=json.dumps(city))
     else:
         link_api_to_tf = os.environ.get("link_api_to_tf")
         r_to_api = requests.post(link_api_to_tf, data=city)
         print(r_to_api.content)
         data_from_api = r_to_api.content.decode("utf-8")
         ip_tf = os.environ.get("ip_tf")
-        r_to_tf = requests.post({0}, ip_tf, {1}, data=data_from_api .format("http://", ":80"))
+        r_to_tf = requests.post('http://{0}:9000'.format(ip_tf), data=data_from_api)
         print(r_to_tf.content)
     return r_to_tf.content
